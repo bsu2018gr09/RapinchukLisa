7508c60834543e610aedcd047d1cf4330c21a692
lisarapinchuk <46031136+lisarapinchuk@users.noreply.github.com>
2019-05-24 03:24:18 +0300

    update 1-3

--- 1-3.cpp
+++ 1-3.cpp
@@ -1,5 +1,5 @@
-﻿//Элементы массива А(N), значения которых – простые числа, 
-//расположить в порядке возрастания, не нарушая порядка следования других элементов.
+﻿//Элементы массива А(N), значения которых – простые числа, расположить в порядке возрастания, 
+//не нарушая порядка следования других элементов.
 
 #include <iostream>
 #include <cmath>
@@ -10,12 +10,27 @@
 using namespace std;
 
 
-void writeArray(int* arr, const int size) {
-	for (int i = 0; i < size; i++) cout << *(arr + i) << " ";
+void writeNumbers(int* min, int* max, int* size) {
+	cout << "Диапазон чисел: \n";
+	cin >> *min >> *max;
+	if (*min > *max) swap(*min, *max);
+	cout << "Размер массива: ";
+	cin >> *size;
+}
+
+void randomInitArray(int* arr, int n, const int min, const int max) {
+	srand(time(0)); 
+	for (int* p = arr; p < arr + n; p++) {
+		*p = min + rand() % (max - min + 1);
+	}
+}
+
+void printArray(int* arr, const int size) {
+	for (int i = 0; i < size; i++) cout << setw(4) <<*(arr + i) << " ";
 	cout << endl;
 }
 
-bool isSimple(int n) {
+bool isPrime(int n) {
 	if (abs(n) == 2) return true;
 	else if (abs(n) <= 1 || abs(n) == 0) return false;
 	int end = sqrt(abs(n));
@@ -23,17 +38,6 @@ bool isSimple(int n) {
 	return true;
 }
 
-void numbers(int& a, int& b, int& size) {
-	cout << "Диапазон чисел (a,b): ";
-	cin >> a >> b;
-	cout << "Размер массива: ";
-	cin >> size;
-}
-
-void randomArray(int* arr, int size, const int min, const int max) {
-	srand(time(0));
-	for (int i = 0; i < size; i++) *(arr + i) = rand() % 10;
-}
 
 void deleteMemory(int* arr) {
 	delete[] arr;
@@ -41,7 +45,7 @@ void deleteMemory(int* arr) {
 }
 
 int* getMemory(int size) {
-	int* arr = new int[size];
+	int* arr = new (nothrow) int[size];
 	if (!arr) exit(1);
 	return arr;
 }
@@ -51,8 +55,8 @@ void sort(int* arr, const int size) {
 		int* now = nullptr;
 		int* bf = nullptr;
 		for (int* j = arr; j < arr + size; j++) {
-			if (now && isSimple(*j) && (*j < *now)) { swap(*now, *j);	bf = now; now = j; }
-			else if (isSimple(*j)) { bf = now; now = j; }
+			if (now && isPrime(*j) && (*j < *now)) { swap(*now, *j);	bf = now; now = j; }
+			else if (isPrime(*j)) { bf = now; now = j; }
 		}
 	}
 }
@@ -61,13 +65,13 @@ int main() {
 
 	setlocale(LC_ALL, "Russian");
 	int size, min, max;
-	numbers(min, max, size);
-	int* arr = getMemory(size);
-	randomArray(arr, size, min, max);
-	writeArray(arr, size);
-	sort(arr, size);
-	writeArray(arr, size);
-	deleteMemory(arr);
+	writeNumbers(&min, &max, &size);
+	int* Arr = getMemory(size);
+	randomInitArray(Arr, size, min, max);
+	printArray(Arr, size);
+	sort(Arr, size);
+	printArray(Arr, size);
+	deleteMemory(Arr);
 	system("pause");
 	return 0;
 }
